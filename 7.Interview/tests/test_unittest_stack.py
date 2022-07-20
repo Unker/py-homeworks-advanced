6ba9eafe86b91ff7b4a7b2537ec3282f41cf3a7c
Кузнецов Ф.И <fkuznetsov@nkbvs.ttn.ru>
2022-07-20 15:51:50 +0300

    add test __eq__

--- 7.Interview/tests/test_unittest_stack.py
+++ 7.Interview/tests/test_unittest_stack.py
@@ -39,6 +39,14 @@ class TestStack(unittest.TestCase):
     def test_dunder_methods(self):
         self.stack = stack.Stack(self.lst)
         ret = self.stack.size()
+        # test __len__
         self.assertEqual(ret, len(self.stack))
+        # test __iter__, __next__
         elements = [el for el in self.stack]
         self.assertEqual(set(self.lst), set(elements))
+        # test __eq__
+        self.stack1 = stack.Stack(self.lst)
+        self.stack2 = stack.Stack(self.lst)
+        self.assertEqual(self.stack1, self.stack2)
+        self.stack1.pop()
+        self.assertNotEqual(self.stack1, self.stack2)
