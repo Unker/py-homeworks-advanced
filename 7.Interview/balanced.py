c6a03e573df590da0606645f7c29765513c0ad59
Кузнецов Ф.И <fkuznetsov@nkbvs.ttn.ru>
2022-07-21 13:32:24 +0300

    fix null str

--- 7.Interview/balanced.py
+++ 7.Interview/balanced.py
@@ -1,29 +1,16 @@
 from stack import Stack
 
-good1 = '(((([{}]))))'
-good2 = '[([])((([[[]]])))]{()}'
-good3 = '{{[()]}}'
-bad1 = '}{}'
-bad2 = '{{[(])]}}'
-bad3 = '[[{())}]'
-
-bad4 = '{[([])((([[[]]])))]{()}]'
-
-
-lefts = '([{'
-rights = ')]}'
+closes = ')]}'
 mirror = {
     '{': '}',
     '[': ']',
     '(': ')',
-    '}': '{',
-    ']': '[',
-    ')': '(',
 }
 
 
 def is_balanced(elements: Stack) -> bool:
-    if elements.size() % 2 != 0:
+    elements = Stack(elements)
+    if elements.size() % 2 != 0 or elements.size() == 0:
         return False
     else:
         ret = True
@@ -31,21 +18,16 @@ def is_balanced(elements: Stack) -> bool:
         right = Stack([])
         while not left.is_empty():
             el = left.pop()
-            if el in rights:
+            if el in closes:
                 right.push(el)
             elif not right.is_empty():
                 if right.pop() != mirror[el]:
                     ret = False
                     break
+            else:
+                ret = False
+                break
 
         return ret
 
 
-print(is_balanced(Stack(bad1)))
-print(is_balanced(Stack(bad2)))
-print(is_balanced(Stack(bad3)))
-print(is_balanced(Stack(bad4)))
-print('*****')
-print(is_balanced(Stack(good1)))
-print(is_balanced(Stack(good2)))
-print(is_balanced(Stack(good3)))
